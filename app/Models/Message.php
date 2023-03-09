8ecf5736e215558f6b1001673bd578e27af3acda
Maria Paula Hernandez <mpaulah24@gmail.com>
2023-03-09 15:47:02 -0500

    Add FIle field in Message model

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -11,6 +11,7 @@ class Message extends Model
     use HasFactory;
     protected $fillable = [
         'text',
+        'file',
         'date',
         'chat_id',
         'user_id',
--- database/migrations/2023_03_06_000902_create_messages_table.php
+++ database/migrations/2023_03_06_000902_create_messages_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
             $table->string('text');
+            $table->string('file');
             $table->datetime('date');
             $table->foreignId('user_id')->constrained('users')->onupdate('cascade')->ondelete('cascade');
             $table->foreignId('chat_id')->constrained('chats')->onupdate('cascade')->ondelete('cascade');
