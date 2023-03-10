08c4c525e3e6f3c6296c9802230b07d9d1480d3c
Maria Paula Hernandez <mpaulah24@gmail.com>
2023-03-09 21:55:04 -0500

    Fixed shhow view to my chats

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -75,7 +75,6 @@ class ChatController extends Controller
     public function findByUser(){
 
         //Find chats where user is the owner 
-
         $chats = Service::join('chats', 'services.id', '=', 'chats.service_id')
         ->join('users', 'users.id', '=', 'chats.guest_user_id')
         ->where('services.user_id', '=', Auth::user()->id)
@@ -89,6 +88,21 @@ class ChatController extends Controller
             )
         ->get();  
 
+        //Find chats where the user is a guest
+        $guest_chats = Service::join('chats', 'services.id', '=', 'chats.service_id')
+        ->join('users', 'users.id', '=', 'services.user_id')
+        ->where('chats.guest_user_id', '=', Auth::user()->id)
+        ->select(
+            'chats.id as chat_id',
+            'chats.created_at as created_at',
+            'services.id as service_id',
+            'services.title as service_title',
+            'users.id as guest_user_id',
+            'users.name as guest_user_name'
+            )
+        ->get();  
+
+        $data['guest_chats'] = $guest_chats;
         $data['chats'] = $chats;
 
         return view('chat.my-chats',$data);
--- resources/views/chat/my-chats.blade.php
+++ resources/views/chat/my-chats.blade.php
@@ -1,48 +1,69 @@
 <x-app-layout>
-  <x-slot name="header">
-      <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-          {{ __('My Chats') }}
-      </h2>
-  </x-slot>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('My Chats') }}
+        </h2>
+    </x-slot>
 
-  <div class="py-12">
+    <div class="py-12">
 
-      @if (session('status'))
-          <div class="mb-4 rounded-lg bg-success-100 py-5 px-6 text-base text-success-700 alert alert-success" role="alert">
-              {{ session('status') }}
-          </div>
-      @endif
+        @if (session('status'))
+            <div class="mb-4 rounded-lg bg-success-100 py-5 px-6 text-base text-success-700 alert alert-success"
+                role="alert">
+                {{ session('status') }}
+            </div>
+        @endif
 
-      <div class="max-w-7xl mx-auto mb-4 sm:px-6 lg:px-8">
-          <a href=#>
-              <x-primary-button>
-                  {{ __('New Chat') }}
-              </x-primary-button>
-          </a>
-      </div>
+        <div class="max-w-7xl mx-auto mb-4 sm:px-6 lg:px-8">
+            <a href=#>
+                <x-primary-button>
+                    {{ __('New Chat') }}
+                </x-primary-button>
+            </a>
+        </div>
 
-      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-              <div class="p-6 text-gray-900 dark:text-gray-100">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
 
-                  @foreach ($chats as $chat)
-                      <div class="my-3 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
-                          style="margin-bottom: 10px;">
-                          <a href=#>
-                              <img class="rounded-t-lg " src="no-img.jpg" alt="" />
-                          </a>
-                          <div class="p-5 ">
-                              <a href={{route('chat.show',['service_id' => $chat->service_id, 'id' => $chat->chat_id])}}>
-                                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                      {{ $chat->service_title }}<h5>
-                              </a>
-                              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $chat->guest_user_name }}
-                              </p>
-                          </div>
-                      </div>
-                  @endforeach
-              </div>
-          </div>
-      </div>
-  </div>
+                    @foreach ($chats as $chat)
+                        <div class="my-3 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+                            style="margin-bottom: 10px;">
+                            <a href=#>
+                                <img class="rounded-t-lg " src="no-img.jpg" alt="" />
+                            </a>
+                            <div class="p-5 ">
+                                <a
+                                    href={{ route('chat.show', ['service_id' => $chat->service_id, 'id' => $chat->chat_id]) }}>
+                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                        {{ $chat->service_title }}<h5>
+                                </a>
+                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                                    {{ $chat->guest_user_name }}
+                                </p>
+                            </div>
+                        </div>
+                    @endforeach
+                    @foreach ($guest_chats as $guest_chat)
+                    <div class="my-3 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+                        style="margin-bottom: 10px;">
+                        <a href=#>
+                            <img class="rounded-t-lg " src="no-img.jpg" alt="" />
+                        </a>
+                        <div class="p-5 ">
+                            <a
+                                href={{ route('chat.show', ['service_id' => $guest_chat->service_id, 'id' => $guest_chat->chat_id]) }}>
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    {{ $guest_chat->service_title }}<h5>
+                            </a>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                                {{ $guest_chat->guest_user_name }}
+                            </p>
+                        </div>
+                    </div>
+                @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
